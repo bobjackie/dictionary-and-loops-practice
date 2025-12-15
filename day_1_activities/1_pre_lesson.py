7cbc511e0755ab08881ae2a69a71a38b3eb1bd90
bobjackie <jackie.aug31@gmail.com>
2025-12-15 20:08:28 +0000

    dictonaries

--- day_1_activities/1_pre_lesson.py
+++ day_1_activities/1_pre_lesson.py
@@ -1,1 +1,34 @@
 # this is what we will use for the video intro to dictionaries
+
+
+#dictonary collection of key value pairs, ordered and changeable. No duplicates 
+
+capitals = {"USA" : "Washington, DC.",
+"India" : "New Delhi", "China" : "Beijing", "Russia" : "Moscow"}
+
+# print(dir(capitals))
+# print(help(capitals))
+print(capitals.get("Japam"))
+
+if capitals.get("Japan"):
+    print("That capital exists")
+else:
+    print("That capital doesn't exist")
+
+capitals.update({"Germany": "Berlin "})
+capitals.update({"USA": "Detroit"})
+
+keys = capitals.keys()
+
+print(keys)
+
+for key in capitals.keys():
+    print(key)
+
+values = capitals.values()
+for value in capitals.values():
+    print(value)
+
+items = capitals.items()
+for key, value in capitals.items():
+    print(f"{key}: {value}")
