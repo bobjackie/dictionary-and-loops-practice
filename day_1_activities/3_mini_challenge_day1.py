62636656e1d8d444428ca7583b8d6bc0c87c1f7f
bobjackie <jackie.aug31@gmail.com>
2025-12-17 18:49:04 +0000

    push

--- day_1_activities/3_mini_challenge_day1.py
+++ day_1_activities/3_mini_challenge_day1.py
@@ -40,54 +40,58 @@ import student_data2
 cps = input("Enter CPS ID: ")
 first_name = input("Enter First name: ")
 last_name = input("Enter Last name: ")
-middle_name = input("Enter Middle name: ")
+full_name = f"{last_name}, {first_name}"
+middle_name = input("Enter middle name, if applicable: ")
 homeroom = input("Enter homeroom:")
 grade_level = input("Enter Grade level: ")
-primary_email = input("Enter Primary email: ")
-secondary_email = input("Enter Secondary email: ")
-
-
+primary_email = input("Enter main email: ")
+secondary_email = input("Enter second email: ")
+# this asks the coder to input the students information and then prints it out 
 # 2. Combine the First and Last name into this format:
     #    "Last, First" 
 
-print(f"{last_name}, {first_name}")
+print(f"{last_name}, {first_name}") #prints out the students full name in the program 
 
 # 3. Store all of the new information into ONE dictionary
     #    that matches the structure of the existing student data.
 
-for student in student_data2.students:
-    if student["CPSID"] == int(cps):
-        print("Error: CPS ID already taken. Student not added.")
-        break
-else:
-    new_student = {
-        "CPSID": int(cps),
-        "Combo,Name": f"{last_name}, {first_name}",
-        "LName": last_name,
-        "FName": first_name,
-        "MName": middle_name,
-        "HR": homeroom,
-        "GL": int(grade_level),
-        "Email": [primary_email, secondary_email]
-    }
-    student_data2.students.append(new_student)
-    print("Student added successfully!")
-    print(f"Total number of students: {len(student_data2.students)}")
-    print("Newly added student record:")
-    print(new_student)
-
+new_student = {
+    "CPS ID": cps,
+    "First Name": first_name,
+    "Last Name": last_name,
+    "Middle Name": middle_name,
+    "Full Name": f"{last_name}, {first_name}",
+    "Homeroom": homeroom,
+    "Grade Level": grade_level,
+    "Primary Email": primary_email,
+    "Secondary Email": secondary_email }
+  
+#This creates a dictonary with all the students information in on the dictonary
 # 4. Add (append) that new dictionary into the main students list.
 
+student_data2.students.append(new_student) #this adds the new stundet to the list of students 
 # 5. After adding the student, the program must:
     #    - Print a confirmation message
     #    - Print the total number of students in the system
     #    - Print the newly added student record
+print("your student was added ") #after student is added it prints out this message 
 
+print(f"\nStudent {full_name} was added!")
+print(f"number of stuents: {len(student_data2.students)}")
 # 6. The program must NOT delete or overwrite any existing students.
+
+for student in student_data2.students:
+    if student["CPS ID"] == ["CPS ID"]:
+        print("Error: CPS ID already exists.")
+        break
+
 
 # 7. If the CPS ID already exists in the system:
         #    - Do NOT add the student
         #    - Display an error message saying the CPS ID is already taken
 
+
+        
+    
 
 
